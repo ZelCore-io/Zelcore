acfa219bad4e074ec80f136668d75cd7bb9138b5
Kyuubi-2709 <66879129+Kyuubi-2709@users.noreply.github.com>
2020-09-11 00:21:06 +0200

    Update newssources.js

--- newssources.js
+++ newssources.js
@@ -1918,7 +1918,7 @@ const newssources = {
     ],
 	om: [
 	{
-        name: "OM Twitter",
+        name: "MANTRA DAO Twitter",
         feed: "https://nitter.net/MANTRADAO/rss",
         site: "https://twitter.com/MANTRADAO",
         about: "MANTRA DAO is a community-governed DeFi platform focusing on Staking, Lending, and Governance. MANTRA DAO leverages the wisdom of the crowd to create a community-governed, transparent, and decentralized ecosystem for web 3.0. Built on Parity Substrate for the Polkadot ecosystem, MANTRA DAO gives financial control back to the people to grow wealth together.",
