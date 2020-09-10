5544142ecf6bc90c5144bbea7173fac8e0c532af
Kyuubi-2709 <66879129+Kyuubi-2709@users.noreply.github.com>
2020-09-11 00:04:19 +0200

    Update coininfo.js

--- coininfo.js
+++ coininfo.js
@@ -9008,7 +9008,7 @@ const coininfo = {
       },
     },
   },
-  MANTRA DAO
+  om: {
   description: "MANTRA DAO is a community-governed DeFi platform focusing on Staking, Lending, and Governance. MANTRA DAO leverages the wisdom of the crowd to create a community-governed, transparent, and decentralized ecosystem for web 3.0. Built on Parity Substrate for the Polkadot ecosystem, MANTRA DAO gives financial control back to the people to grow wealth together.",
     total_supply: 125333333,
     circulating_supply: 109180888,
